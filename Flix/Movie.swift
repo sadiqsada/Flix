ff043837549fa1b29604af95eb4ce834ae7ad23d
Md Sadiq Sada <samin.sada@gmail.com>
2021-02-14 14:08:05 -0500

    Basic Flix App Done

---
+++ Flix/Movie.swift
@@ -0,0 +1,23 @@
+//
+//  Movie.swift
+//  Flix
+//
+//  Created by Md Sadiq Sada on 2/13/21.
+//
+
+import UIKit
+
+class Movie: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Flix/MovieCellTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MovieCellTableViewCell.swift
+//  Flix
+//
+//  Created by Md Sadiq Sada on 2/13/21.
+//
+
+import UIKit
+
+class MovieCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
