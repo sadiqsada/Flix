7bf53759decf2ca833f25a2069f2d78bece0860e
Md Sadiq Sada <samin.sada@gmail.com>
2021-02-20 19:15:56 -0500

    basic app done - unnecessary spacing between grid cells needs to be fixed

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Md Sadiq Sada on 2/20/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
